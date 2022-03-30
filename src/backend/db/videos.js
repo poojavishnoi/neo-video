b88dfbaea2ce0f9fef7f4628c1f1c0f3fc9f00a4
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-30 23:15:12 +0530

    Added some data in backend

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,206 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "zxtfaNjI9_0",
+    title: "Prateek Kuhad - Just A Word | Official Music Video",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    creator: "Prateek Kuhad",
+    categoryName: "Indie Music"
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "VA0V8rQpqwQ",
+    title: "Kho Gaye Hum Kahaan (Live in Ahmedabad)",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Prateek Kuhad",
+    categoryName: "Indie Music"
   },
+  {
+    _id: "sCP5emCrGEE",
+    title: "Tum jab paas",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Prateek Kuhad",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "cTwsW54rM3A",
+    title: "Tere Hi Hum - Prateek Kuhad | Official Lyric Video ✨🚀",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Prateek Kuhad",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "PJWemSzExXs",
+    title: "BAARISHEIN (Studio) Anuv Jain",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Anuv Jain",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "SmaY7RfBgas",
+    title: "Anuv Jain - GUL (Studio)",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Anuv Jain",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "vA86QFrXoho",
+    title: "Anuv Jain - ALAG AASMAAN (a song on the ukulele)",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Anuv Jain",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "Y2zc2IeVX_g",
+    title: "OCEAN by Anuv Jain (a song on the ukulele)",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Anuv Jain",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "CvV5P-mgaR8",
+    title: "Anuv Jain - MAULA (Studio)",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Anuv Jain",
+    categoryName: "Indie Music"
+  },
+  {
+    _id: "COFgTynydQE",
+    title: "Kina Grannis - Can't Help Anuv Jain - MAULA (Studio)Falling In Love (Piano Version) Official Stream",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Kina Grannis",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "j6t9EQ6YBJE",
+    title: "Louis Armstrong - Wonderful World (cover) | Frizzell D'Souza    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Frizzell D'Souza ",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "YY8FzSQQwgo",
+    title: "Grow As We Go - Ben Platt (cover)    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Frizzell D'Souza ",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "EO9cuJMsEEI",
+    title: "Stand by Me - Ben E. King (acoustic cover) | Frizzell D'souza    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Frizzell D'Souza ",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "TaLMCWOh1pY",
+    title: "Ed Sheeran - Tenerife Sea (cover)    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Frizzell D'Souza ",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "xTHuKrjz4Q8",
+    title: "Sean Kingston - Beautiful Girls (acoustic cover)    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Frizzell D'Souza ",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "Gl2dJ9HyLBk",
+    title: "Anuv Jain - Ocean (cover) | Frizzell D'souza    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Frizzell D'Souza ",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "i8fa3JIrgpY",
+    title: "At My Worst - Pink Sweat$ - Vocal and acoustic guitar cover Ft. Renee Foy    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "8atglOOig8A",
+    title: "Photograph - Ed Sheeran - Vocal Cover ft. Renee Foy    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "V2a-sl4l3_4",
+    title: "Let her Go - Passenger - Cover ft. Renee Foy (Vocal / Acoustic fingerstyle)    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "mzI22Oz-Pho",
+    title: "Before You Go - Lewis Capaldi - Cover (Vocal / Fingerstyle)    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Acoustic Cover"
+  },
+  {
+    _id: "k_ZRVM3aYZc",
+    title: "Flute Version: O Re Piya | Aaja Nachle | Salim-Sulaiman | Jaideep Sahni | Vijay Tambe    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Instrumental Music"
+  },
+  {
+    _id: "4twsvQvrhvc",
+    title: "Yeh Haseen Vadiyan | Sitar cover | Bhagirath Bhatt (Instrumental) | Ankit Dhupper    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Instrumental Music"
+  },
+  {
+    _id: "Z96KMymDHkQ",
+    title: "Kabira/dil diyan gallan/Hawayein/yara teri yari ko/ilahi Bollywood Flute Mashup Vishal Gendle Flute    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Instrumental Music"
+  },
+  {
+    _id: "fd2e-mmeU_w",
+    title: "Bollywood Mashup on Flute | Bubai    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Instrumental Music"
+  },
+  {
+    _id: "Z5d_BL-0miw",
+    title: "Mind blowing instrumental music by AR Rahman || Music Meditation || YouTube Music    ",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "renee foy",
+    categoryName: "Instrumental Music"
+  },
+
+
+
 ];
