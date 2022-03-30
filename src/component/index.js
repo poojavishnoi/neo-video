d3c21c1286bab0c13318c5a3bb787b39a43b9168
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-30 23:27:05 +0530

    added exports in one file

---
+++ src/component/index.js
@@ -0,0 +1,7 @@
+export { CgPlayList } from "react-icons/cg";
+export { BiSearch  } from "react-icons/bi";
+export {BsClockHistory} from "react-icons/bs"
+export {AiOutlineUser,AiOutlineLike} from 'react-icons/ai'
+export {MdWatchLater} from 'react-icons/md'
+export {Footer} from './Footer'
+export {Header} from './Header'
