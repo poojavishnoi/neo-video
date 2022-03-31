fb52c63ef0763e64895c1e58a28331d480dd9bf7
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-03-31 23:56:02 +0530

    feat: Added like feature

---
+++ src/context/like-context.js
@@ -0,0 +1,46 @@
+import { useReducer, useContext, createContext } from "react";
+
+const likeContext = createContext(null);
+
+const LikeContextProvider = ({ children }) => {
+  const reducerFunction = (likeState, { type, payload }) => {
+    switch (type) {
+      case "LIKE":
+        let index = likeState.likeList.findIndex(
+          (ele) => ele._id === payload._id
+        );
+        if (index === -1) {
+          return {
+            ...likeState,
+            likeList: [...likeState.likeList, { ...payload }],
+            likeCount: likeState.likeCount + 1,
+          };
+        } else {
+          return likeState;
+        }
+      case "DISLIKE":
+        return {
+          ...likeState,
+          likeList: likeState.likeList.filter((c) => c.id !== payload.id),
+          likeCount: likeState.likeCount - 1,
+        };
+      default:
+        return { likeState };
+    }
+  };
+
+  const [{ likeList, likeCount }, likeDispatch] = useReducer(reducerFunction, {
+    likeList: [],
+    likeCount: 0,
+  });
+
+  return (
+    <likeContext.Provider value={{ likeList, likeCount, likeDispatch }}>
+      {children}
+    </likeContext.Provider>
+  );
+};
+
+const useLike = () => useContext(likeContext);
+
+export { useLike, LikeContextProvider };
