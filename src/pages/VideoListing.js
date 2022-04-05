da93963d3899c1b54ac9f1804886b528970bd137
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-04-06 02:41:53 +0530

    made use of video context

--- src/pages/VideoListing.js
+++ src/pages/VideoListing.js
@@ -2,22 +2,15 @@ import React from "react";
 import { useEffect, useState } from "react";
 import "../style/videolisting.css";
 import { VideoCard } from "../component/";
+import { useVideo } from "../context/video-context";
 
 export function VideoListing() {
-  const [videoList, setVideoList] = useState([]);
+  const {videoList} = useVideo();
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetch("/api/videos");
-      const result = await data.json();
-      setVideoList(result.videos);
-    }
-    fetchData();
-  }, []);
   return (
     <div className="explore_main_container">
       {videoList.map((videoItem) => {
-        return <VideoCard watch={false} liked={false} data={videoItem} />;
+        return <VideoCard videoList={videoList} watch={false} liked={false} data={videoItem} />;
       })}
     </div>
   );
