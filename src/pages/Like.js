37091d2f3059c151e3b853080906f25dd8cb6505
Pooja Vishnoi <pooja.k.vishnoi@gmail.com>
2022-04-07 00:00:38 +0530

    added alt

--- src/pages/Like.js
+++ src/pages/Like.js
@@ -9,7 +9,7 @@ export function Like() {
   return (
     <>
       {likeList.length === 0 ? (
-        <img src="https://cdn.dribbble.com/users/453325/screenshots/5573953/empty_state.png?compress=1&resize=800x600&vertical=top" />
+        <img src="https://cdn.dribbble.com/users/453325/screenshots/5573953/empty_state.png?compress=1&resize=800x600&vertical=top" alt=""/>
       ) : (
         <div className="like_main_container">
           {likeList.map((videoItem) => {
